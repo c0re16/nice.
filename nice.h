6307811d49b45d42833a3a729751a56311657257
c0re16 <cgball13@gmail.com>
2019-10-15 11:42:45 -0400

    Add files via upload

---
+++ nice.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define nicee ;
+#define Nicee main()
+#define nIcee {
+#define niCee }
+#define nicEe int
+#define NIcee =
+#define nICee (
+#define niCEe )
+#define NICee <=
+#define nICEe i++
+#define NICEe i
+
+#define niceE !=
+#define NiceE 69
+#define nIceE "%d\n",i
+#define niCeE "Nice\n"
+#define nicEE else
+#define NIceE printf
+#define nICeE return
+#define niCEE 0
+#define nICEE for
+#define NICEE =
+
---
+++ nicee.c
@@ -0,0 +1,23 @@
+#include "nice.h"
+
+
+
+
+
+nicEe Nicee nIcee 
+
+nicEe NICEe nicee 
+
+nICEE nICee nicEe NICEe NICEE niCEE nicee NICEe NICee 100 nicee nICEe niCEe nIcee
+
+if nICee NICEe niceE NiceE niCEe nIcee 
+
+NIceE nICee nIceE niCEe nicee niCee
+
+nicEE nIcee NIceE nICee niCeE niCEe nicee niCee
+
+niCee
+
+niCee
+
+
